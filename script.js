60c3cc3aaf515fead57ed0104457480c73dc0ee4
Dec <126972375+Declan-J@users.noreply.github.com>
2025-11-21 13:44:16 +0000

    Created square grid using width/height calculations

--- script.js
+++ script.js
@@ -2,11 +2,14 @@
 const container = document.querySelector(".container");
 
 //Variable number of squares per row
-let rowCount = 2;
-
+let rowCount = 16;
 //Square rowCount to create square grid
 for (let i=0; i<(rowCount*rowCount); i++) {
     const div = document.createElement("div");
+
+    //Set Width & Height to be 100% / rowCount
+    div.style.height = `calc(100% / ${rowCount})`
+    div.style.width = `calc(100% / ${rowCount})`
 
     //Add grid class to new elements 
     div.classList.toggle("grid-cell")
