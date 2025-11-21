3e93a3cb1e5bfdff1db624fda65d9c4dc73a78bf
Dec <126972375+Declan-J@users.noreply.github.com>
2025-11-21 01:37:57 +0000

    Using variable counter to create divs, set container size.
    TODO: Make divs display in an equal grid format

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-/*
-CREATE A 16x16 grid of square divs using JS ONLY!
-- Use Flexbox to make divs appear as a grid (Not CSS Grid this time!)
-*/
-
+//Set up container to push the grid into
 const container = document.querySelector(".container");
 
-//create all divs
-for (let i=0; i<16; i++) {
+//Variable number of squares per row
+let rowCount = 2;
+
+//Square rowCount to create square grid
+for (let i=0; i<(rowCount*rowCount); i++) {
     const div = document.createElement("div");
+
+    //Add grid class to new elements 
     div.classList.toggle("grid-cell")
+    
+    //Push to container
     container.appendChild(div);
 }
-
-
-/*
-For each div
-
-Apply style to div to make it appear in a grid.
-
-    border
-    width
-    height
-
-    display flex 
-    justify content
-    flex row, wrap
-*/
