e258aa7a7c75ddfe28526c3a4bde870f01934712
Dec <126972375+Declan-J@users.noreply.github.com>
2025-11-22 13:16:34 +0000

    Change color to gray on hovering squares

--- script.js
+++ script.js
@@ -2,17 +2,24 @@
 const container = document.querySelector(".container");
 
 //Variable number of squares per row
-let rowCount = 16;
+let girdSize = 16;
 //Square rowCount to create square grid
-for (let i=0; i<(rowCount*rowCount); i++) {
+for (let i=0; i<(girdSize*girdSize); i++) {
     const div = document.createElement("div");
 
-    //Set Width & Height to be 100% / rowCount
-    div.style.height = `calc(100% / ${rowCount})`
-    div.style.width = `calc(100% / ${rowCount})`
+    //Set Width & Height to be 100% / girdSize
+    div.style.height = `calc(100% / ${girdSize})`
+    div.style.width = `calc(100% / ${girdSize})`
+
+    div.addEventListener('mouseover', (event) => {
+        if (event.button == 0) 
+        {
+            event.currentTarget.style.backgroundColor = "gray"
+        }
+    });
 
     //Add grid class to new elements 
-    div.classList.toggle("grid-cell")
+    div.classList.toggle("grid-cell");
     
     //Push to container
     container.appendChild(div);
