4ca0dfe38d3285f1fbe9d9f6f1b57e06a71b9205
Dec <126972375+Declan-J@users.noreply.github.com>
2025-11-22 17:51:19 +0000

    Logic for creating and resetting grid
    Fixed a bug where drawing was stuck that occurred when accidentally dragging cells

--- script.js
+++ script.js
@@ -6,38 +6,77 @@ let isDrawing = false;
 
 //Variable number of squares per row
 let girdSize = 16;
-//Square rowCount to create square grid
-for (let i=0; i<(girdSize*girdSize); i++) {
-    const div = document.createElement("div");
+createGrid(girdSize);
 
-    //Set Width & Height to be 100% / girdSize
-    div.style.height = `calc(100% / ${girdSize})`
-    div.style.width = `calc(100% / ${girdSize})`
-
-    div.addEventListener('mousedown', (e) => {
-        isDrawing = true;
-        changeColor(e, "gray");
-    });
-
-    div.addEventListener('mousemove', (e) => {
-        if(isDrawing){
-            changeColor(e, "gray");
-        }
-    });
-
-    div.addEventListener('mouseup', () => {
-        isDrawing = false;
-    })
-
-    //Add grid class to new elements 
-    div.classList.toggle("grid-cell");
-    
-    //Push to container
-    container.appendChild(div);
+function changeColor(mouseEvent, color) {
+    if (mouseEvent.button == 0) {
+        mouseEvent.currentTarget.style.backgroundColor = `${color}`
+    }
 }
 
-function changeColor (mouseEvent, color) {
-    if (mouseEvent.button == 0) {
-        mouseEvent.currentTarget.style.backgroundColor = `${color}`   
+function resetGrid() {
+    const allGridCells = document.querySelectorAll(".grid-cell");
+    allGridCells.forEach(cell => {
+        cell.style.backgroundColor = "";
+    });
+    isDrawing = false;
+}
+
+function createNewGrid() {
+    let newGridSize = prompt("Please enter the number (1-100) of squares per side for the new grid. (Limit: 100)", 16);
+
+    if (newGridSize > 0 && newGridSize < 100) {
+        //remove existing grid
+        removeGrid();
+
+        //generate new grid in the container.
+        createGrid(newGridSize);
+        isDrawing = false;
+    }
+    else alert("Invalid value. Please enter a number between 1 - 100.")
+}
+
+function removeGrid() {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function createGrid(girdSize) {
+    //Square gridSize to create square grid
+    for (let i = 0; i < (girdSize * girdSize); i++) {
+        const div = document.createElement("div");
+
+        //Set Width & Height to be 100% / girdSize
+        div.style.height = `calc(100% / ${girdSize})`
+        div.style.width = `calc(100% / ${girdSize})`
+
+        div.addEventListener('mousedown', (e) => {
+            isDrawing = true;
+            changeColor(e, "gray");
+        });
+
+        div.addEventListener('mousemove', (e) => {
+            if (isDrawing) {
+                changeColor(e, "gray");
+            }
+        });
+
+        div.addEventListener('mouseup', () => {
+            isDrawing = false;
+        })
+
+        //Add grid class to new elements 
+        div.classList.toggle("grid-cell");
+
+        //Prevent unintended dragging of grid cells
+        div.setAttribute("draggable", "false")
+        div.addEventListener('dragstart', (e) => {
+            e.preventDefault();
+            isDrawing = false;
+        })
+
+        //Push to container
+        container.appendChild(div);
     }
 }
