843710d59e8a4ed1d286b05279d199acbfa94ab3
Dec <126972375+Declan-J@users.noreply.github.com>
2025-11-22 16:25:32 +0000

    Click or hold left mouse to draw on grid

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-//Set up container to push the grid into
+//Set up the container of the grid
 const container = document.querySelector(".container");
+
+//Used to draw when clicking or holding left mouse button
+let isDrawing = false;
 
 //Variable number of squares per row
 let girdSize = 16;
@@ -11,16 +14,30 @@ for (let i=0; i<(girdSize*girdSize); i++) {
     div.style.height = `calc(100% / ${girdSize})`
     div.style.width = `calc(100% / ${girdSize})`
 
-    div.addEventListener('mouseover', (event) => {
-        if (event.button == 0) 
-        {
-            event.currentTarget.style.backgroundColor = "gray"
+    div.addEventListener('mousedown', (e) => {
+        isDrawing = true;
+        changeColor(e, "gray");
+    });
+
+    div.addEventListener('mousemove', (e) => {
+        if(isDrawing){
+            changeColor(e, "gray");
         }
     });
+
+    div.addEventListener('mouseup', () => {
+        isDrawing = false;
+    })
 
     //Add grid class to new elements 
     div.classList.toggle("grid-cell");
     
     //Push to container
     container.appendChild(div);
+}
+
+function changeColor (mouseEvent, color) {
+    if (mouseEvent.button == 0) {
+        mouseEvent.currentTarget.style.backgroundColor = `${color}`   
+    }
 }
